2a64c8808f911dd86b8f28cbe40dd58024448fd8
Mondelezz <pankov.egor26032005@yandex.ru>
2023-10-28 22:12:52 +0300

    Add all my files

---
+++ Quantum/Controllers/WebSocketsController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Quantum.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class WebSocketsController : ControllerBase
+    {
+    }
+}
