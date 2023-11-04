4f8161964b93dc85627a2634cc4d8ce98d6569ef
Mondelezz <121812529+Mondelezz@users.noreply.github.com>
2023-11-04 18:30:50 +0300

    Add files via upload

---
+++ Interfaces/IWebSocket.cs
@@ -0,0 +1,10 @@
+﻿using System.Net.WebSockets;
+
+namespace Quantum.Interfaces
+{
+    public interface IWebSocket
+    {
+        public Task WebSocketRequestAsync(WebSocket webSocket);
+        public Task EchoAsync(WebSocket webSocket);
+    }
+}
