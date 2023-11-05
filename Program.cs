6d53245c2e20820ae005f43bb6a3e1933f8460c3
Mondelezz <121812529+Mondelezz@users.noreply.github.com>
2023-11-05 12:55:11 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,12 +1,24 @@
+using Quantum.Mapping;
 using Quantum.Interfaces;
 using Quantum.Services;
+using Quantum.Interfaces.UserInterface;
+using Quantum.Services.UserServices;
+using Quantum.Data;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
+
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddAutoMapper(typeof(Program).Assembly);
+builder.Services.AddAutoMapper(typeof(AppMappingProfile));
 builder.Services.AddScoped<IWebSocket, WebSocketServices>();
-
+builder.Services.AddScoped<IUserHub, UserHubService>();
+builder.Services.AddDbContext<DataContext>(options =>
+{
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
 var app = builder.Build();
 
 
@@ -25,14 +37,8 @@ app.UseWebSockets(webSocketOptions);
 
 
 app.UseHttpsRedirection();
-
-app.UseAuthorization();
 app.UseRouting();
-app.UseEndpoints(endpoints =>
-{
-    endpoints.MapControllers();
-});
-
+app.UseAuthorization();
 app.MapControllers();
 
 app.Run();
