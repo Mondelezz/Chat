ae197ed38723393ce727200fc740103d1d9fe193
Mondelezz <121812529+Mondelezz@users.noreply.github.com>
2023-11-05 12:52:59 +0300

    Add files via upload

---
+++ Interfaces/UserInterface/IUserHub.cs
@@ -0,0 +1,9 @@
+﻿using Quantum.Models.DTO;
+
+namespace Quantum.Interfaces.UserInterface
+{
+    public interface IUserHub
+    {
+        public Task EnterUserInformation(UserDTO userDTO);
+    }
+}
