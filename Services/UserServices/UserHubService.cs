8776448bf1f189ca0fe22910657407888c7d5dba
Mondelezz <121812529+Mondelezz@users.noreply.github.com>
2023-11-05 12:54:53 +0300

    Add files via upload

---
+++ Services/UserServices/UserHubService.cs
@@ -0,0 +1,42 @@
+﻿using AutoMapper;
+using Quantum.Data;
+using Quantum.Interfaces.UserInterface;
+using Quantum.Models;
+using Quantum.Models.DTO;
+
+namespace Quantum.Services.UserServices
+{
+    public class UserHubService : IUserHub
+    {
+        private readonly ILogger<UserHubService> _logger;
+        private readonly DataContext _dataContext;
+        private readonly IMapper _mapper;
+        public UserHubService(DataContext dataContext, IMapper mapper, ILogger<UserHubService> logger)
+        { 
+            _dataContext = dataContext;
+            _mapper = mapper;
+            _logger = logger;
+        }
+
+        public async Task EnterUserInformation(UserDTO userDTO)
+        {
+            try
+            {
+                if (userDTO != null)
+                {
+                    User user = _mapper.Map<User>(userDTO);
+                    user.UserId = Guid.NewGuid();
+                    await _dataContext.Users.AddAsync(user);
+                    await _dataContext.SaveChangesAsync();
+                    _logger.Log(LogLevel.Information, "Данные были сохранены в базу данных");
+                }
+            }
+            catch (Exception ex)
+            {
+                _logger.Log(LogLevel.Error, $"Исключение при сохранении в базу данных: {ex.Message}");
+                throw;
+            }
+        }
+
+    }
+}
