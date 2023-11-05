1627049b52443ba3221fe18b185235d5e5a9500e
Mondelezz <121812529+Mondelezz@users.noreply.github.com>
2023-11-05 12:52:29 +0300

    Add files via upload

---
+++ Controllers/UserController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Quantum.Interfaces.UserInterface;
+using Quantum.Models.DTO;
+
+namespace Quantum.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly IUserHub _userHub;
+        public UserController(IUserHub userHub)
+        {
+            _userHub = userHub;
+        }
+        [HttpPost("reg")]
+        public async Task EnterUserInformation([FromBody] UserDTO userDTO)
+        {
+            try
+            {
+                await _userHub.EnterUserInformation(userDTO);                
+            }
+            catch (Exception)
+            {
+                HttpContext.Response.StatusCode = StatusCodes.Status400BadRequest;                
+            }
+        }
+    }
+}
