6ce5b81ee58ef68a6c9b1346b074dc0837a5e57b
Mondelezz <121812529+Mondelezz@users.noreply.github.com>
2023-11-05 15:35:20 +0300

    Update IWebSocket.cs

--- Interfaces/IWebSocket.cs
+++ Interfaces/IWebSocket.cs
@@ -4,7 +4,7 @@ namespace Quantum.Interfaces
 {
     public interface IWebSocket
     {
-        public Task WebSocketRequestAsync(WebSocket webSocket);
+        public Task HandleWebSocketRequestAsync(WebSocket webSocket);
         public Task EchoAsync(WebSocket webSocket);
     }
 }
