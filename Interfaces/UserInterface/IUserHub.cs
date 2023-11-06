fe264b80b5983701e993b281930a13c4a8de0c03
Mondelezz <121812529+Mondelezz@users.noreply.github.com>
2023-11-06 23:10:32 +0300

    Delete Interfaces directory

--- Interfaces/UserInterface/IUserHub.cs
+++
@@ -1,9 +0,0 @@
-using Quantum.Models.DTO;
-
-namespace Quantum.Interfaces.UserInterface
-{
-    public interface IUserHub
-    {
-        public Task EnterUserInformation(RegistrationUserDTO registrationUserDTO);
-    }
-}
