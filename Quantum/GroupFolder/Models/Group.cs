d7c0a90d1d51f23f6453a7c1c4e1acefb2676fb5
Mondelezz <pankov.egor26032005@yandex.ru>
2024-01-04 17:47:15 +0300

    Количество участников, первичная инициализация

--- Quantum/GroupFolder/Models/Group.cs
+++ Quantum/GroupFolder/Models/Group.cs
@@ -5,12 +5,20 @@ namespace Quantum.GroupFolder.Models
 {
     public class Group
     {
+        public int Id { get; set; }
+        // Название
         public string NameGroup { get; set; } = string.Empty;
+        // Описание
         public string? DescriptionGroup { get; set; } = string.Empty;
+        // Id
         public Guid GroupId { get; set; }
-        public List<UserInfoOutput> Users { get; set; } = new List<UserInfoOutput>();
+        // Участники
+        public List<UserInfoOutput> Members { get; set; } = new List<UserInfoOutput>();
+        // Статус доступности(открытая/закрытая) 
         public bool StatusAccess { get; set; } = false;
+        // Количество участников
         public int CountMembers { get; set; } 
+        // Ссылка - приглашение
         public string LinkInvitation { get; set; } = string.Empty;
     }
 }
